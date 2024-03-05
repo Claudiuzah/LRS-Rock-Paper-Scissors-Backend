77b1660a0466aa51c7f1541bf5b634acfac633aa
Claudiuzah <zahariac7@gmail.com>
2024-03-05 22:32:08 +0200

    commit 1

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from api.users.users import users_router
+from api.websocket_manager.ws import ConnectionManager
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(users_router)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+manager = ConnectionManager()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket, client_id)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        while True:
+            message = await websocket.receive_text()
+            await manager.broadcast(message, list(client_id))
+    except WebSocketDisconnect:
+        await manager.disconnect(websocket, client_id)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
