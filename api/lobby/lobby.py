a7f99244b2b57d362f74b62e92d751b7be7332ea
Claudiuzah <zahariac7@gmail.com>
2024-03-13 23:14:23 +0200

    lobby endpoints start

--- api/lobby/lobby.py
+++ api/lobby/lobby.py
@@ -2,13 +2,41 @@ from fastapi import APIRouter
 from api.lobby.utils import get_lobby_data, create_lobby
 from api.lobby.models import CreateLobby
 
-lobby_router = APIRouter()
-
-
-@lobby_router.post("/api/lobby/create")
+lobby_router = APIRouter(
+    prefix="/api/lobby",
+    tags=["lobby"]
+)
+@lobby_router.post("/create")
 async def create_lobby(lobby_data: CreateLobby):
     lobby = get_lobby_data(lobby_data)
     if lobby is None:
         lobby = create_lobby(lobby_data)
 
     return lobby
+
+# def get_user(user_id: str, db = Depends(get_db)):
+#     user = db.query(User).filter_by(id=user_id).first() referinta pt get lobby
+
+@lobby_router.get("/onlineplayers")
+async def online_players():
+    pass
+
+@lobby_router.put("/invite")
+async def invite():
+    pass
+
+@lobby_router.post("/user/choice")
+async def user_choice():
+    pass
+
+@lobby_router.put("/playerstatus")
+async def player_status():
+    pass
+
+@lobby_router.get("/points")
+async def points():
+    pass
+
+@lobby_router.get("/{lobby_id}")
+async def get_lobby(lobby_id):
+    pass
