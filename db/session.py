4b85b95ca4dacb83665672316b5043507642ee51
Your Name <you@example.com>
2024-04-10 14:54:25 +0300

    UserProfileUUIDVer

--- db/session.py
+++ db/session.py
@@ -3,13 +3,9 @@ from sqlalchemy.orm import sessionmaker
 
 DB_PASSWORD = "0000"
 DB_NAME = "RPSDB"
-# engine = create_engine(f"postgresql://postgres:{DB_PASSWORD}@localhost:5432/{DB_NAME}")
-#
 
 DATABASE_URL = f"postgresql://postgres:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
 
-# Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
-# Create a SessionLocal class, which is a factory for creating database sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
