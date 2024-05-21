948952a7a8eaa9a6a1bf4d2ed3e89de196ca4559
Gabriel Capusneac <capusneacgabriel2004@gmail.com>
2024-05-21 15:26:32 +0300

    ws implement+raw user stats & leaderboard

--- main.py
+++ main.py
@@ -82,7 +82,6 @@ async def read_root(user: dict = Depends(get_current_user)):
 if __name__ == "__main__":
     #     uvicorn.run(app="main:app", host=HOST, port=int(PORT), reload=True)
     #     # uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
-    #     # 172.16.1.89
     #     # pip freeze > requirements.txt
     #     # pip install -r requirements.txt  For installing requirements
     import uvicorn
